d721ea928b71ab675fc76ed84acdf1e4810b7367
Dan Retegan <danretegan@yahoo.com>
2024-05-07 12:23:06 +0300

    Get Weather API Call

--- src/scripts/forecast.js
+++ src/scripts/forecast.js
@@ -1,8 +1,20 @@
-const key = 'S7G2LbP0Y0yEFG1MikAzostUxeEZK6Pl';
+const apiKey = 'S7G2LbP0Y0yEFG1MikAzostUxeEZK6Pl';
 
+//TODO get weather information:
+const getWeather = async cityKey => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${cityKey}?apikey=${apiKey}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+// TODO get city information:
 const getCity = async city => {
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-  const query = `?apikey=${key}&q=${city}`;
+  const query = `?apikey=${apiKey}&q=${city}`;
+
   const response = await fetch(base + query);
   const data = await response.json();
   return data[0];
@@ -10,8 +22,9 @@ const getCity = async city => {
 
 getCity('Bistrita')
   .then(data => {
+    return getWeather(data.Key);
+  })
+  .then(data => {
     console.log(data);
   })
-  .catch(err => {
-    console.log(err);
-  });
+  .catch(err => console.log(err));
