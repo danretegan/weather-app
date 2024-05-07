5b14bd4ba3e20f0b30417d5b4ec1df06e4aa4bfe
Dan Retegan <danretegan@yahoo.com>
2024-05-07 13:43:03 +0300

    Updating the UI

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,37 @@
 import { getCity, getWeather } from './forecast.js';
 
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = data => {
+  console.log(data); //! Vezi in consola cheile de care e nevoie!
+  const cityDetails = data.cityDetails;
+  const weather = data.weather;
+
+  //!   Update the details template:
+
+  details.innerHTML = `
+  <h5 class="my-3">${cityDetails.LocalizedName}</h5>
+  <h6>${cityDetails.AdministrativeArea.LocalizedName}</h6>
+  <div class="my-3">${weather.WeatherText}</div>
+  <div class="display-4 my-4">
+    <span>${weather.Temperature.Metric.Value}</span>
+    <span>&deg;C</span>
+  </div>
+  `;
+
+  //! remove the d-none class if it's pressent:
+
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+};
 
 const updateCity = async city => {
   const cityDetails = await getCity(city);
   const weather = await getWeather(cityDetails.Key);
 
-  //TODO   Object Shorthand Notation:
   return { cityDetails, weather };
 };
 
@@ -19,6 +44,6 @@ cityForm.addEventListener('submit', e => {
 
   //   update the ui with the new city:
   updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
