244c5fe129500860a2516b630268dcde37eab030
Dan Retegan <danretegan@yahoo.com>
2024-05-07 17:24:06 +0300

    Weather Icons & images

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,10 +3,12 @@ import { getCity, getWeather } from './forecast.js';
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = data => {
-  //!   Destructuring:
   const { cityDetails, weather } = data;
+  console.log(data);
 
   details.innerHTML = `
   <h5 class="my-3">${cityDetails.LocalizedName}</h5>
@@ -17,6 +19,18 @@ const updateUI = data => {
     <span>&deg;C</span>
   </div>
   `;
+
+  //! update the night/day & icon images:
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+
+  let timeSrc = null;
+  if (weather.IsDayTime) {
+    timeSrc = 'img/day.svg';
+  } else {
+    timeSrc = 'img/night.svg';
+  }
+  time.setAttribute('src', timeSrc);
 
   if (card.classList.contains('d-none')) {
     card.classList.remove('d-none');
